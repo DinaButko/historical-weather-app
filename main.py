e8d2b96613756b7f78cc901ea204ddce00d7930f
Dina Butko <butkodina@gmail.com>
2024-02-06 08:55:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template
+import pandas
 
 app = Flask("Website")
+
 
 @app.route("/")
 def home():
     """if we put template in tutorials folder we don't write path"""
     return render_template("home.html")
 
+
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     """if we put template in tutorials folder we don't write path"""
     """<> what user puts as for request """
-    temperature = 23
     df = pandas.read_csv("")
     temperature = df.station(date)
-    #return render_template("about.html")
-    return{"staion": station,
-           "date": date,
-           "temperature": temperature}
+    weather_result = {"staion": station,
+                      "date": date,
+                      "temperature": temperature}
+    return weather_result
+
 
 app.run(debug=True)
